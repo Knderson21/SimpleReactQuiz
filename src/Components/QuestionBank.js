47488a0f04617d2ed46145bec02a4b134188855e
Knderson21 <knderson21@gmail.com>
2024-08-10 17:15:07 -0400

    questions and scores

---
+++ src/Components/QuestionBank.js
@@ -0,0 +1,33 @@
+const qBank = [
+    {
+        id: 1,
+        question: "When did Seventeen debut?",
+        options: ["7/17/2017", "6/13/2013", "5/26/2015", "2/14/2016"],
+        answer: "5/26/2015",
+    },
+    {
+        id: 2,
+        question: "Which of the following is NOT one of Seventeen's subunits?",
+        options: ["Performance", "Rap", "Vocal", "Hip Hop"],
+        answer: "Rap",
+    },
+    {
+        id: 3,
+        question: "How many members are in Seventeen?",
+        options: ["17", "23", "15", "13"],
+        answer: "13",
+    },
+    {
+        id: 4,
+        question: "What are Seventeen's official colors?",
+        options: ["Baby Pink and Sky Blue", "Rose Quartz and Serenity", "Blush Sparkle and Sapphire Diamond", "Twinkle and Shimmer"],
+        answer: "Rose Quartz and Serenity",
+    },
+    {
+        id: 5,
+        question: "Which of the following songs is NOT a title track?",
+        options: ["Fear", "Hit", "Clap", "Cheers"],
+        answer: "Cheers",
+    }
+]
+export default qBank;
